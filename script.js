2e45db015345810f10093f12eee139122ff89217
mmkhan <mariatechfuture@gmail.com>
2020-08-20 12:30:30 -0400

    started navbar

---
+++ script.js
@@ -0,0 +1,20 @@
+/* init Jarallax */
+jarallax(document.querySelectorAll('.jarallax'));
+jarallax(document.querySelectorAll('.jarallax-keep-img'), { keepImg: true });
+
+document.addEventListener('DOMContentLoaded', function() {
+	var elems = document.querySelectorAll('.sidenav');
+	var instances = M.Sidenav.init(elems, options);
+});
+
+// Initialize collapsible (uncomment the lines below if you use the dropdown variation)
+// var collapsibleElem = document.querySelector('.collapsible');
+// var collapsibleInstance = M.Collapsible.init(collapsibleElem, options);
+
+// Or with jQuery
+
+$(document).ready(function() {
+	$('.sidenav').sidenav();
+});
+
+var instance = M.Sidenav.getInstance(elem);
